38341fd199010146134120ac37008bd316c095ed
Kevin Adler <adler187@gmail.com>
2018-11-25 17:21:25 -0600

    Vuetify the design
    Use a datepicker and time picker. Design needs some work yet

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,43 +1,103 @@
 <template>
-  <div>
-    <h1>When do you turn 1 billion seconds old?</h1>
-      <input
-        type="text"
-        name="birthdate"
-        v-model="birthdate"
-        placeholder="Birth Date"/>
-      <select v-model="format">
-        <option v-for="format in formats" :key="format">{{format}}</option>
-      </select>
-    <h2
-      v-if="billion_seconds"
-      id="birthdate">
-      You'll be 1 billion seconds old on {{billion_seconds}}
-    </h2>
-  </div>
+  <v-layout row>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="blue" dark>
+          <v-toolbar-title>When do you turn 1 billion seconds old?</v-toolbar-title>
+        </v-toolbar>
+        <h1>When do you turn 1 billion seconds old?</h1>
+        <h2>{{message}}</h2>
+        <v-menu
+          :close-on-content-click="false"
+          v-model="date_picker_open"
+          :nudge-right="40"
+          lazy
+          transition="scale-transition"
+          offset-y
+          min-width="290px">
+          <v-text-field
+            slot="activator"
+            label="Birth Date"
+            v-model="birthdate"
+            prepend-icon="event"
+            readonly>
+          </v-text-field>
+          <v-date-picker
+            v-model="birthdate"
+            @input="date_picker_open = false">
+          </v-date-picker>
+        </v-menu>
+        <v-menu
+          ref="menu"
+          :close-on-content-click="false"
+          v-model="time_picker_open"
+          :nudge-right="40"
+          :return-value.sync="birthtime"
+          lazy
+          transition="scale-transition"
+          offset-y
+          max-width="290px"
+          min-width="290px">
+          <v-text-field
+            slot="activator"
+            v-model="birthtime"
+            label="Birth Time"
+            prepend-icon="access_time"
+            readonly>
+          </v-text-field>
+          <v-time-picker
+            v-if="time_picker_open"
+            v-model="birthtime"
+            @change="$refs.menu.save(birthtime)">
+          </v-time-picker>
+        </v-menu>
+        <!-- <v-flex xs12 sm6 md3>
+          <v-select
+            :items="formats"
+            v-model="format"
+            label="Date Format">
+          </v-select>
+        </v-flex> -->
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
 import moment from 'moment'
 export default {
-  data () {
-    return {
-      birthdate: '',
-      format: 'YYYY-MM-DD',
-      formats: [
-        'YYYY-MM-DD',
-        'MM/DD/YYYY',
-        'DD/MM/YYYY'
-      ]
-    }
-  },
+  data: () => ({
+    interval_id: null,
+    // now: moment(),
+    birthdate: new Date().toISOString().substr(0, 10),
+    birthtime: '00:00',
+    format: 'YYYY-MM-DD',
+    formats: [
+      'YYYY-MM-DD',
+      'MM/DD/YYYY',
+      'DD/MM/YYYY'
+    ],
+    date_picker_open: false,
+    time_picker_open: false
+  }),
   methods: {
   },
   computed: {
     billion_seconds () {
-      let m = moment(this.birthdate, this.format)
-      if (m.isValid()) {
-        return m.add(1000000000, 'seconds').format(this.format)
+      return moment(this.birthdate + ' ' + this.birthtime).add(1000000000, 's')
+    },
+    seconds_until_billion () {
+      console.log('seconds')
+      return this.billion_seconds.diff(this.now, 's')
+    },
+    message () {
+      if (this.billion_seconds.isValid()) {
+        let formatted = this.billion_seconds.format(this.format)
+        if (this.seconds_until_billion > 0) {
+          return `You will turn 1 billion seconds old on ${formatted} in ${this.seconds_until_billion} seconds.`
+        } else {
+          return `You turned 1 billion seconds old on ${formatted}`
+        }
       } else {
         return ''
       }
