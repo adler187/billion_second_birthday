912ed09258229be061fad039dbc676c8f8d9680e
Kevin Adler <adler187@gmail.com>
2018-11-25 12:44:42 -0600

    Convert to Vue.js
    This just recreates the original page in Vue; improvements to follow.

---
+++ src/components/Calculator.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h1>When do you turn 1 billion seconds old?</h1>
+      <input
+        type="text"
+        name="birthdate"
+        v-model="birthdate"
+        placeholder="Birth Date"/>
+      <select v-model="format">
+        <option v-for="format in formats" :key="format">{{format}}</option>
+      </select>
+    <h2
+      v-if="billion_seconds"
+      id="birthdate">
+      You'll be 1 billion seconds old on {{billion_seconds}}
+    </h2>
+  </div>
+</template>
+
+<script>
+import moment from 'moment'
+export default {
+  data () {
+    return {
+      birthdate: '',
+      format: 'YYYY-MM-DD',
+      formats: [
+        'YYYY-MM-DD',
+        'MM/DD/YYYY',
+        'DD/MM/YYYY'
+      ]
+    }
+  },
+  methods: {
+  },
+  computed: {
+    billion_seconds () {
+      let m = moment(this.birthdate, this.format)
+      if (m.isValid()) {
+        return m.add(1000000000, 'seconds').format(this.format)
+      } else {
+        return ''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
